d9a2ae04391ccc9ad0d1987dcc62cbaa1adab9a1
TodeschiniDeveloper <mateus.todeschini.developer@gmail.com>
2026-09-03 10:14:28 -0300

    add project

---
+++ Projeto Isocaedro/Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace Projeto_Isocaedro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawPanel = new System.Windows.Forms.Panel();
+            this.palettePanel = new System.Windows.Forms.Panel();
+            this.panelBlack = new System.Windows.Forms.Panel();
+            this.panelGray = new System.Windows.Forms.Panel();
+            this.panelPurple = new System.Windows.Forms.Panel();
+            this.panelOrange = new System.Windows.Forms.Panel();
+            this.panelMagente = new System.Windows.Forms.Panel();
+            this.panelCyan = new System.Windows.Forms.Panel();
+            this.panelYellow = new System.Windows.Forms.Panel();
+            this.panelBlue = new System.Windows.Forms.Panel();
+            this.panelGreen = new System.Windows.Forms.Panel();
+            this.panelRed = new System.Windows.Forms.Panel();
+            this.tbX = new System.Windows.Forms.TrackBar();
+            this.tbY = new System.Windows.Forms.TrackBar();
+            this.tbScale = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblX = new System.Windows.Forms.Label();
+            this.lblY = new System.Windows.Forms.Label();
+            this.lblScale = new System.Windows.Forms.Label();
+            this.palettePanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.tbX)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbY)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbScale)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawPanel
+            // 
+            this.drawPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.drawPanel.Location = new System.Drawing.Point(249, 35);
+            this.drawPanel.Name = "drawPanel";
+            this.drawPanel.Size = new System.Drawing.Size(308, 338);
+            this.drawPanel.TabIndex = 0;
+            this.drawPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawPanel_Paint);
+            this.drawPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawPanel_MouseClick);
+            // 
+            // palettePanel
+            // 
+            this.palettePanel.Controls.Add(this.panelBlack);
+            this.palettePanel.Controls.Add(this.panelGray);
+            this.palettePanel.Controls.Add(this.panelPurple);
+            this.palettePanel.Controls.Add(this.panelOrange);
+            this.palettePanel.Controls.Add(this.panelMagente);
+            this.palettePanel.Controls.Add(this.panelCyan);
+            this.palettePanel.Controls.Add(this.panelYellow);
+            this.palettePanel.Controls.Add(this.panelBlue);
+            this.palettePanel.Controls.Add(this.panelGreen);
+            this.palettePanel.Controls.Add(this.panelRed);
+            this.palettePanel.Location = new System.Drawing.Point(582, 22);
+            this.palettePanel.Name = "palettePanel";
+            this.palettePanel.Size = new System.Drawing.Size(200, 100);
+            this.palettePanel.TabIndex = 1;
+            // 
+            // panelBlack
+            // 
+            this.panelBlack.BackColor = System.Drawing.Color.Black;
+            this.panelBlack.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelBlack.Location = new System.Drawing.Point(27, 45);
+            this.panelBlack.Name = "panelBlack";
+            this.panelBlack.Size = new System.Drawing.Size(18, 17);
+            this.panelBlack.TabIndex = 3;
+            this.panelBlack.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelGray
+            // 
+            this.panelGray.BackColor = System.Drawing.Color.Gray;
+            this.panelGray.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelGray.Location = new System.Drawing.Point(3, 45);
+            this.panelGray.Name = "panelGray";
+            this.panelGray.Size = new System.Drawing.Size(18, 17);
+            this.panelGray.TabIndex = 3;
+            this.panelGray.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelPurple
+            // 
+            this.panelPurple.BackColor = System.Drawing.Color.Purple;
+            this.panelPurple.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelPurple.Location = new System.Drawing.Point(171, 22);
+            this.panelPurple.Name = "panelPurple";
+            this.panelPurple.Size = new System.Drawing.Size(18, 17);
+            this.panelPurple.TabIndex = 3;
+            this.panelPurple.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelOrange
+            // 
+            this.panelOrange.BackColor = System.Drawing.Color.Orange;
+            this.panelOrange.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelOrange.Location = new System.Drawing.Point(147, 22);
+            this.panelOrange.Name = "panelOrange";
+            this.panelOrange.Size = new System.Drawing.Size(18, 17);
+            this.panelOrange.TabIndex = 3;
+            this.panelOrange.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelMagente
+            // 
+            this.panelMagente.BackColor = System.Drawing.Color.Magenta;
+            this.panelMagente.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelMagente.Location = new System.Drawing.Point(123, 22);
+            this.panelMagente.Name = "panelMagente";
+            this.panelMagente.Size = new System.Drawing.Size(18, 17);
+            this.panelMagente.TabIndex = 3;
+            this.panelMagente.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelCyan
+            // 
+            this.panelCyan.BackColor = System.Drawing.Color.Cyan;
+            this.panelCyan.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelCyan.Location = new System.Drawing.Point(99, 22);
+            this.panelCyan.Name = "panelCyan";
+            this.panelCyan.Size = new System.Drawing.Size(18, 17);
+            this.panelCyan.TabIndex = 3;
+            this.panelCyan.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelYellow
+            // 
+            this.panelYellow.BackColor = System.Drawing.Color.Yellow;
+            this.panelYellow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelYellow.Location = new System.Drawing.Point(75, 22);
+            this.panelYellow.Name = "panelYellow";
+            this.panelYellow.Size = new System.Drawing.Size(18, 17);
+            this.panelYellow.TabIndex = 3;
+            this.panelYellow.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelBlue
+            // 
+            this.panelBlue.BackColor = System.Drawing.Color.Blue;
+            this.panelBlue.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelBlue.Location = new System.Drawing.Point(51, 22);
+            this.panelBlue.Name = "panelBlue";
+            this.panelBlue.Size = new System.Drawing.Size(18, 17);
+            this.panelBlue.TabIndex = 2;
+            this.panelBlue.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelGreen
+            // 
+            this.panelGreen.BackColor = System.Drawing.Color.Lime;
+            this.panelGreen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelGreen.Location = new System.Drawing.Point(27, 22);
+            this.panelGreen.Name = "panelGreen";
+            this.panelGreen.Size = new System.Drawing.Size(18, 17);
+            this.panelGreen.TabIndex = 1;
+            this.panelGreen.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // panelRed
+            // 
+            this.panelRed.BackColor = System.Drawing.Color.Red;
+            this.panelRed.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.panelRed.Location = new System.Drawing.Point(3, 22);
+            this.panelRed.Name = "panelRed";
+            this.panelRed.Size = new System.Drawing.Size(18, 17);
+            this.panelRed.TabIndex = 0;
+            this.panelRed.Click += new System.EventHandler(this.Form1_Click);
+            // 
+            // tbX
+            // 
+            this.tbX.Location = new System.Drawing.Point(41, 22);
+            this.tbX.Maximum = 180;
+            this.tbX.Minimum = -180;
+            this.tbX.Name = "tbX";
+            this.tbX.Size = new System.Drawing.Size(104, 45);
+            this.tbX.TabIndex = 2;
+            this.tbX.ValueChanged += new System.EventHandler(this.tbX_ValueChanged);
+            // 
+            // tbY
+            // 
+            this.tbY.Location = new System.Drawing.Point(41, 67);
+            this.tbY.Maximum = 180;
+            this.tbY.Minimum = -180;
+            this.tbY.Name = "tbY";
+            this.tbY.Size = new System.Drawing.Size(104, 45);
+            this.tbY.TabIndex = 3;
+            this.tbY.ValueChanged += new System.EventHandler(this.tbY_ValueChanged);
+            // 
+            // tbScale
+            // 
+            this.tbScale.Location = new System.Drawing.Point(41, 130);
+            this.tbScale.Maximum = 250;
+            this.tbScale.Minimum = 30;
+            this.tbScale.Name = "tbScale";
+            this.tbScale.Size = new System.Drawing.Size(104, 45);
+            this.tbScale.TabIndex = 4;
+            this.tbScale.Value = 100;
+            this.tbScale.ValueChanged += new System.EventHandler(this.tbScale_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(654, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // lblX
+            // 
+            this.lblX.AutoSize = true;
+            this.lblX.Location = new System.Drawing.Point(65, 195);
+            this.lblX.Name = "lblX";
+            this.lblX.Size = new System.Drawing.Size(78, 13);
+            this.lblX.TabIndex = 6;
+            this.lblX.Text = "Girar Eixo X: 0°";
+            // 
+            // lblY
+            // 
+            this.lblY.AutoSize = true;
+            this.lblY.Location = new System.Drawing.Point(68, 230);
+            this.lblY.Name = "lblY";
+            this.lblY.Size = new System.Drawing.Size(78, 13);
+            this.lblY.TabIndex = 7;
+            this.lblY.Text = "Girar Eixo Y: 0°";
+            // 
+            // lblScale
+            // 
+            this.lblScale.AutoSize = true;
+            this.lblScale.Location = new System.Drawing.Point(68, 257);
+            this.lblScale.Name = "lblScale";
+            this.lblScale.Size = new System.Drawing.Size(71, 13);
+            this.lblScale.TabIndex = 8;
+            this.lblScale.Text = "Escala: 100%";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScale);
+            this.Controls.Add(this.lblY);
+            this.Controls.Add(this.lblX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbScale);
+            this.Controls.Add(this.tbY);
+            this.Controls.Add(this.tbX);
+            this.Controls.Add(this.palettePanel);
+            this.Controls.Add(this.drawPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.palettePanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.tbX)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbY)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tbScale)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawPanel;
+        private System.Windows.Forms.Panel palettePanel;
+        private System.Windows.Forms.Panel panelBlue;
+        private System.Windows.Forms.Panel panelGreen;
+        private System.Windows.Forms.Panel panelRed;
+        private System.Windows.Forms.Panel panelBlack;
+        private System.Windows.Forms.Panel panelGray;
+        private System.Windows.Forms.Panel panelPurple;
+        private System.Windows.Forms.Panel panelOrange;
+        private System.Windows.Forms.Panel panelMagente;
+        private System.Windows.Forms.Panel panelCyan;
+        private System.Windows.Forms.Panel panelYellow;
+        private System.Windows.Forms.TrackBar tbX;
+        private System.Windows.Forms.TrackBar tbY;
+        private System.Windows.Forms.TrackBar tbScale;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblX;
+        private System.Windows.Forms.Label lblY;
+        private System.Windows.Forms.Label lblScale;
+    }
+}
+
---
+++ Projeto Isocaedro/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto_Isocaedro
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public Color cores(int r, int g, int b)
+        {
+            Color cor = new Color();
+            cor = Color.FromArgb(r, g, b);
+            return cor;
+        }
+
+        public Pen caneta(int r, int g, int b)
+        {
+            Pen caneta = new Pen(cores(r, g, b));
+            return caneta;
+        }
+
+        public Pen caneta(int r, int g, int b, int esp, float[] v)
+        {
+            Pen caneta = new Pen(cores(r, g, b), esp);
+            caneta.DashPattern = v;
+            return caneta;
+        }
+
+        public void pintaPonto(PaintEventArgs e, Pen caneta, int x, int y)
+        {
+            e.Graphics.DrawLine(caneta, x, y, x + 1, y);
+        }
+
+        public void pintaLinha(PaintEventArgs e, Pen caneta, int x, int y, int x1, int y1)
+        {
+            e.Graphics.DrawLine(caneta, x, y, x1, y1);
+        }
+
+        public void pintaRetangulo(PaintEventArgs e, Pen caneta, int x, int y, int x1, int y1)
+        {
+            e.Graphics.DrawRectangle(caneta, x, y, x1, y1);
+        }
+
+        public void desenhaPoligono(PaintEventArgs e, Pen caneta, Point[] pontos)
+        {
+            e.Graphics.DrawPolygon(caneta, pontos);
+        }
+
+        public void PreenchePoligono(PaintEventArgs e, SolidBrush fundo, Point[] pontos)
+        {
+            e.Graphics.FillPolygon(fundo, pontos);
+        }
+
+        public void PreenchePoligono(PaintEventArgs e, HatchBrush fundo, Point[] pontos)
+        {
+            e.Graphics.FillPolygon(fundo, pontos);
+        }
+
+        public void PreenchePoligono(PaintEventArgs e, TextureBrush fundo, Point[] pontos)
+        {
+            e.Graphics.FillPolygon(fundo, pontos);
+        }
+
+        public SolidBrush preen_Area(Color cor)
+        {
+            SolidBrush pincel = new SolidBrush(cor);
+            return pincel;
+        }
+
+        public HatchBrush preen_Area(HatchStyle hachura, Color corL, Color corF)
+        {
+            HatchBrush pincel = new HatchBrush(hachura, corL, corF);
+            return pincel;
+        }
+
+        public TextureBrush preen_Area(string imagem)
+        {
+            Bitmap bmp = new Bitmap(imagem);
+            TextureBrush pincel = new TextureBrush(bmp);
+            return pincel;
+        }
+
+        public Point[] poligono(int[] x, int[] y)
+        {
+            int tamanho_x_y = x.Length;
+            Point[] pontos = new Point[tamanho_x_y];
+
+            for (int i = 0; i < tamanho_x_y; i++)
+            {
+                Point point1 = new Point(x[i], y[i]);
+                pontos[i] = point1;
+            }
+
+            return pontos;
+        }
+        private static readonly PointF[] BaseVertices = new PointF[]
+        {
+            new PointF( 0.0f,     0.57735f),
+            new PointF(-0.5f,    -0.28868f),
+            new PointF( 0.5f,    -0.28868f),
+            new PointF( 0.0f,     1.15470f),
+            new PointF( 1.0f,     0.57735f),
+            new PointF( 1.0f,    -0.57735f),
+            new PointF( 0.0f,    -1.15470f),
+            new PointF(-1.0f,    -0.57735f),
+            new PointF(-1.0f,     0.57735f),
+        };
+
+        private Face[] faces = new Face[]
+        {
+            new Face(0, 1, 2),
+            new Face(0, 8, 1),
+            new Face(0, 2, 4),
+            new Face(1, 6, 2),
+            new Face(0, 3, 8),
+            new Face(0, 4, 3),
+            new Face(2, 5, 4),
+            new Face(1, 7, 6),
+            new Face(2, 6, 5),
+            new Face(1, 8, 7),
+        };
+
+        private const float BASE_SCALE = 120f;
+        private Color selectedColor = Color.Red;
+        
+        private Point[] TransformVertices()
+        {
+            float scale = BASE_SCALE * (tbScale.Value / 100f);
+            double angleX = tbX.Value * Math.PI / 180.0;
+            double angleY = tbY.Value * Math.PI / 180.0;
+
+            float cx = drawPanel.Width / 2f;
+            float cy = drawPanel.Height / 2f;
+
+            Point[] result = new Point[BaseVertices.Length];
+
+            for (int i = 0; i < BaseVertices.Length; i++)
+            {
+                float x0 = BaseVertices[i].X;
+                float y0 = BaseVertices[i].Y;
+
+                double rx = x0 * Math.Cos(angleX) - y0 * Math.Sin(angleX);
+                double ry = x0 * Math.Sin(angleX) + y0 * Math.Cos(angleX);
+
+                double projectedX = rx * Math.Cos(angleY);
+                double projectedY = ry * Math.Cos(angleY) + rx * Math.Sin(angleY) * 0.3;
+
+                int x = (int)(cx + (float)projectedX * scale);
+                int y = (int)(cy - (float)projectedY * scale);
+
+                result[i] = new Point(x, y);
+            }
+
+            return result;
+        }
+        private void drawPanel_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            Point[] pts = TransformVertices();
+
+            // Usa a sua primitiva 'caneta' de borda
+            Pen canetaBorda = caneta(0, 0, 0);
+
+            for (int i = 0; i < faces.Length; i++)
+            {
+                Face f = faces[i];
+
+                int[] vx = new int[] { pts[f.VertexIndices[0]].X, pts[f.VertexIndices[1]].X, pts[f.VertexIndices[2]].X };
+                int[] vy = new int[] { pts[f.VertexIndices[0]].Y, pts[f.VertexIndices[1]].Y, pts[f.VertexIndices[2]].Y };
+                Point[] tri = poligono(vx, vy);
+
+                SolidBrush pincelPreenchimento = preen_Area(f.FillColor);
+
+                PreenchePoligono(e, pincelPreenchimento, tri);
+                desenhaPoligono(e, canetaBorda, tri);
+
+                int centroideX = (tri[0].X + tri[1].X + tri[2].X) / 3;
+                int centroideY = (tri[0].Y + tri[1].Y + tri[2].Y) / 3;
+
+                e.Graphics.DrawString((i + 1).ToString(), this.Font, Brushes.Black, centroideX, centroideY);
+            }
+        }
+
+        private void drawPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            Point[] pts = TransformVertices();
+
+            for (int i = 0; i < faces.Length; i++)
+            {
+                Face f = faces[i];
+                Point a = pts[f.VertexIndices[0]];
+                Point b = pts[f.VertexIndices[1]];
+                Point c = pts[f.VertexIndices[2]];
+
+                if (PointInTriangle(e.Location, a, b, c))
+                {
+                    f.FillColor = selectedColor;
+                    drawPanel.Invalidate();
+                    return;
+                }
+            }
+        }
+
+        private void tbX_ValueChanged(object sender, EventArgs e)
+        {
+            lblX.Text = "Girar Eixo X: " + tbX.Value + "°";
+            drawPanel.Invalidate();
+        }
+
+        private void tbY_ValueChanged(object sender, EventArgs e)
+        {
+            lblY.Text = "Girar Eixo Y: " + tbY.Value + "°";
+            drawPanel.Invalidate();
+        }
+
+        private void tbScale_ValueChanged(object sender, EventArgs e)
+        {
+            lblScale.Text = "Escala: " + tbScale.Value + "%";
+            drawPanel.Invalidate();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            Panel swatch = sender as Panel;
+            if (swatch != null)
+            {
+                // Identifica a caixa selecionada pelo Name e define a cor usando a primitiva 'cores'
+                switch (swatch.Name)
+                {
+                    case "panelRed":
+                        selectedColor = cores(255, 0, 0);
+                        break;
+                    case "panelGreen":
+                        selectedColor = cores(0, 255, 0);
+                        break;
+                    case "panelBlue":
+                        selectedColor = cores(0, 0, 255);
+                        break;
+                    case "panelYellow":
+                        selectedColor = cores(255, 255, 0);
+                        break;
+                    case "panelCyan":
+                        selectedColor = cores(0, 255, 255);
+                        break;
+                    case "panelMagenta":
+                        selectedColor = cores(255, 0, 255);
+                        break;
+                    case "panelOrange":
+                        selectedColor = cores(255, 165, 0);
+                        break;
+                    case "panelPurple":
+                        selectedColor = cores(128, 0, 128);
+                        break;
+                    case "panelBlack":
+                        selectedColor = cores(0, 0, 0);
+                        break;
+                    case "panelGray":
+                        selectedColor = cores(128, 128, 128);
+                        break;
+                    default:
+                        selectedColor = cores(swatch.BackColor.R, swatch.BackColor.G, swatch.BackColor.B);
+                        break;
+                }
+            }
+        }
+        private static bool PointInTriangle(PointF p, PointF a, PointF b, PointF c)
+        {
+            float d1 = Sign(p, a, b);
+            float d2 = Sign(p, b, c);
+            float d3 = Sign(p, c, a);
+
+            bool temNegativo = (d1 < 0) || (d2 < 0) || (d3 < 0);
+            bool temPositivo = (d1 > 0) || (d2 > 0) || (d3 > 0);
+
+            return !(temNegativo && temPositivo);
+        }
+
+        private static float Sign(PointF p1, PointF p2, PointF p3)
+        {
+            return (p1.X - p3.X) * (p2.Y - p3.Y) - (p2.X - p3.X) * (p1.Y - p3.Y);
+        }
+    }
+    public class Face
+    {
+        public int[] VertexIndices;
+        public Color FillColor;
+
+        public Face(int a, int b, int c)
+        {
+            VertexIndices = new int[] { a, b, c };
+            FillColor = Color.WhiteSmoke;
+        }
+    }
+}
